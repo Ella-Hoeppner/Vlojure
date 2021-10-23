214e71a9ef0cfb069c4b9cf7b636d9bf036c5437
Ella Hoeppner <ellahoeppner@gmail.com>
2021-10-22 20:51:36 -0400

    Adding replace icon

--- src/clj/vlojure/svgs.clj
+++ src/clj/vlojure/svgs.clj
@@ -1,6 +1,7 @@
 (ns vlojure.svgs
   (:require [clojure.java.io :as java-io]
-            [dali.io :as io]))
+            [dali.io :as io]
+            [vlojure.constants :as c]))
 
 (defn rgb
   "Takes an integer `color` between 0 and 0xffffff, inclusive, and returns a
@@ -8,14 +9,14 @@
   [color]
   (str "#" (format "%06x" color)))
 
-
 ; Constants used to define SVGs
 (def PI Math/PI)
 (def undo-radius 30)
 (def undo-arrow-width 0.8)
 (def undo-arrow-length (/ 0.8 (Math/sqrt 2)))
-(def foreground (rgb 0xff0000))
-(def highlight (rgb 0x00ff00))
+(def foreground-color (rgb 0xff0000))
+(def highlight-color (rgb 0x00ff00))
+(def text-color (rgb 0x0000ff))
 (def output-directory "./resources/public/svgs/")
 
 ; helper functions
@@ -48,19 +49,43 @@
   [(+ 50 (* radius (Math/cos angle)))
    (+ 50 (* radius (Math/sin angle)))])
 
+(defn outline-circle [args pos radius outline-factor]
+  (let [stroke-width (* radius outline-factor)]
+    [:circle (merge args
+                    {:fill "none"
+                     :stroke-width (* 2 stroke-width)
+                     :class "foreground"})
+     pos
+     (- radius stroke-width)]))
+
+(defn text [[x y] font-size text]
+  [:text {:x x :y y
+          :font-color text-color
+          :font-family "monoid-bold"
+          :dominant-baseline "middle"
+          :text-anchor "middle"
+          :font-size font-size}
+   text])
+
 
 ; "documents" defines all SVGs that will be exported when "export" is run
 (def documents
-  ; TODO: replace literal-fn-replace enclose vector-enclose fn-enclose let-enclose comment
+  ; TODO: literal-fn-replace enclose vector-enclose fn-enclose let-enclose comment
   (let []
     (mapv
      (fn [[name doc-fragments]]
        [name
         (vec
          (concat [:dali/page
-                  {:width 100 :height 100}]
+                  {:width 100 :height 100}
+                  [:defs
+                   [:style {:type "text/css"}
+                    "@font-face {
+                                  font-family: monoid-bold;
+                                  src: url('../styles/monoid-bold.ttf');
+                                }"]]]
                  doc-fragments))])
-     [["undo" [(path {:stroke highlight
+     [["undo" [(path {:stroke highlight-color
                       :class "highlight"
                       :stroke-width 12
                       :stroke-linecap "round"
@@ -70,7 +95,7 @@
                       {:r undo-radius
                        :large? true
                        :end-pos (circle-pos undo-radius (* 1.2 PI))}])
-               [:polygon {:fill highlight
+               [:polygon {:fill highlight-color
                           :class "highlight"}
                 (circle-pos (* (inc undo-arrow-width) undo-radius) (* 1.25 PI))
                 (circle-pos (* (- 1 undo-arrow-width) undo-radius) (* 1.25 PI))
@@ -79,7 +104,7 @@
                       (circle-pos undo-radius (* 1.25 PI))
                       [-1 1]
                       (repeat (* undo-radius undo-arrow-length)))]]]
-      ["redo" [(path {:stroke highlight
+      ["redo" [(path {:stroke highlight-color
                       :class "highlight"
                       :stroke-width 12
                       :stroke-linecap "round"
@@ -89,13 +114,39 @@
                       {:r undo-radius
                        :large? true
                        :end-pos (circle-pos undo-radius (* 0.5 PI))}])
-               [:polygon {:fill highlight
+               [:polygon {:fill highlight-color
                           :class "highlight"}
                 (circle-pos (* (inc undo-arrow-width) undo-radius) (* 1.75 PI))
                 (circle-pos (* (- 1 undo-arrow-width) undo-radius) (* 1.75 PI))
                 (mapv +
                       (circle-pos undo-radius (* 1.75 PI))
-                      (repeat (* undo-radius undo-arrow-length)))]]]])))
+                      (repeat (* undo-radius undo-arrow-length)))]]]
+      ["replace" [[:polyline {:stroke highlight-color
+                              :class "highlight"
+                              :stroke-width 4
+                              :stroke-linecap "round"}
+                   (list [40 50] [55 50])]
+                  [:polygon {:fill highlight-color
+                             :class "highlight"}
+                   [52.5 40]
+                   [52.5 60]
+                   [62.5 50]]
+                  [:circle {:fill foreground-color
+                            :class "foreground"}
+                   [20 50]
+                   15
+                   c/bubble-thickness]
+                  [:circle {:fill foreground-color
+                            :class "foreground"}
+                   [80 50]
+                   15
+                   c/bubble-thickness]
+                  (text [20 50]
+                        20
+                        "x")
+                  (text [80 50]
+                        20
+                        "y")]]])))
 
 (defn render-document
   "Takes in a dali `document` and a `filename`, and saves the document as an
