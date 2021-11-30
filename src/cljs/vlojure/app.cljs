df96f0a0825e13ec801f5fdffc0a822e6ab441c9
Ella Hoeppner <ellahoeppner@gmail.com>
2021-11-30 14:30:05 -0500

    Removing 'attr' system in vlojure.app

--- src/cljs/vlojure/app.cljs
+++ src/cljs/vlojure/app.cljs
@@ -38,6 +38,8 @@
 ;;; together the pages and provides functionality for dealing with user input.
 
 (defonce pages (atom {}))
+(defonce active-page (atom nil))
+(defonce mouse (atom nil))
 
 (defn register-page! [key page]
   (swap! pages
@@ -52,36 +54,21 @@
   (doseq [page (keys @pages)]
     (apply page-action (conj args action page))))
 
-(defonce app-state (atom {}))
-
-(defn attr [key]
-  (get @app-state key))
-
-(defn set-attr! [key value]
-  (swap! app-state
-         #(assoc % key value))
-  value)
-
-(defn update-attr! [key value]
-  (swap! app-state
-         #(update % key value)))
-
 (defn enter-page [page]
-  (let [last-page (attr :page)]
-    (page-action last-page :exit))
+  (page-action @active-page :exit)
   (page-action page :enter)
-  (set-attr! :page page))
+  (reset! active-page page))
 
 (defn resize-html-elements []
   (all-pages-action :resize-html))
 
 (defn get-mouse-zone []
-  (page-action (attr :page)
+  (page-action @active-page
                :mouse-zone
-               (:mouse @app-state)))
+               @mouse))
 
 (defn mouse-dragging? []
-  (> (:drag-dist (attr :mouse)) c/min-drag-dist))
+  (> (:drag-dist @mouse) c/min-drag-dist))
 
 (defn render-top-left-button-background [& [highlighted-background?]]
   (let [current-app-rect (app-rect)
@@ -101,7 +88,7 @@
                      c/upper-corner-zone-radius)
                   (inc (Math/sqrt 2)))
         base-circle-pos (add-points app-pos
-                                         (scale-point unit radius))
+                                    (scale-point unit radius))
         background-color (if highlighted-background?
                            (:highlight (color-scheme))
                            (:foreground (color-scheme)))]
@@ -109,24 +96,24 @@
                        (u/prop-range c/settings-zone-icon-spokes true))]
       (draw-line base-circle-pos
                  (add-points base-circle-pos
-                                  (scale-point (angle-point angle)
-                                                    (* radius
-                                                       c/settings-zone-icon-spoke-length-factor)))
+                             (scale-point (angle-point angle)
+                                          (* radius
+                                             c/settings-zone-icon-spoke-length-factor)))
                  (* radius
                     c/settings-zone-icon-spoke-width-factor)
                  (:text (color-scheme))
                  :menu))
     (draw-circle (assoc base-circle-pos
-                            :radius (* radius
-                                       c/settings-zone-icon-radius-factor))
-                     (:text (color-scheme))
-                     :menu)
+                        :radius (* radius
+                                   c/settings-zone-icon-radius-factor))
+                 (:text (color-scheme))
+                 :menu)
     (draw-circle (assoc base-circle-pos
-                            :radius (* radius
-                                       c/settings-zone-icon-radius-factor
-                                       c/settings-zone-icon-inner-radius-factor))
-                     background-color
-                     :menu)))
+                        :radius (* radius
+                                   c/settings-zone-icon-radius-factor
+                                   c/settings-zone-icon-inner-radius-factor))
+                 background-color
+                 :menu)))
 
 (defn render-top-left-back-button []
   (let [current-app-rect (app-rect)
@@ -135,29 +122,29 @@
                      c/upper-corner-zone-radius)
                   (inc (Math/sqrt 2)))
         base-circle-pos (add-points app-pos
-                                         (scale-point unit radius))
+                                    (scale-point unit radius))
         tip (add-points base-circle-pos
-                             {:x (- (* radius
-                                       c/back-icon-left-length-factor))})
+                        {:x (- (* radius
+                                  c/back-icon-left-length-factor))})
         width (* radius c/back-icon-width-factor)
         arrow-size (* radius c/back-icon-tip-length-factor)]
     (draw-line tip
-                   (add-points base-circle-pos
-                                    {:x (* radius
-                                           c/back-icon-right-length-factor)})
+               (add-points base-circle-pos
+                           {:x (* radius
+                                  c/back-icon-right-length-factor)})
+               width
+               (:text (color-scheme))
+               :menu)
+    (draw-polyline [(add-points tip
+                                {:x arrow-size
+                                 :y (- arrow-size)})
+                    tip
+                    (add-points tip
+                                {:x arrow-size
+                                 :y arrow-size})]
                    width
                    (:text (color-scheme))
-                   :menu)
-    (draw-polyline [(add-points tip
-                                         {:x arrow-size
-                                          :y (- arrow-size)})
-                        tip
-                        (add-points tip
-                                         {:x arrow-size
-                                          :y arrow-size})]
-                       width
-                       (:text (color-scheme))
-                       :menu)))
+                   :menu)))
 
 (defn render-top-left-invalid-button []
   (let [current-app-rect (app-rect)
@@ -166,29 +153,29 @@
                      c/upper-corner-zone-radius)
                   (inc (Math/sqrt 2)))
         base-circle-pos (add-points app-pos
-                                         (scale-point unit radius))
+                                    (scale-point unit radius))
         base-offset (scale-point unit
-                                      (* (Math/sqrt 0.5)
-                                         c/new-icon-size
-                                         radius))]
+                                 (* (Math/sqrt 0.5)
+                                    c/new-icon-size
+                                    radius))]
     (doseq [offset [base-offset (update base-offset :x -)]]
       (draw-line (add-points base-circle-pos
-                                      (scale-point offset -1))
-                     (add-points base-circle-pos
-                                      offset)
-                     (* radius c/new-icon-width)
-                     (:text (color-scheme))
-                     :menu))))
+                             (scale-point offset -1))
+                 (add-points base-circle-pos
+                             offset)
+                 (* radius c/new-icon-width)
+                 (:text (color-scheme))
+                 :menu))))
 
 (defn render-app-state []
   (let [current-app-rect (app-rect)
         mouse-zone (get-mouse-zone)]
     (draw-rect current-app-rect
-                        (:background (color-scheme))
-                        :background)
-    (page-action (attr :page)
+               (:background (color-scheme))
+               :background)
+    (page-action @active-page
                  :render
-                 (assoc (attr :mouse)
+                 (assoc @mouse
                         :dragging? (mouse-dragging?))
                  mouse-zone)
     (update-svgs)))
@@ -197,26 +184,25 @@
   (let [delta (get-delta)]
     (when-not (zero? c/scroll-speed)
       (update-global-attr! :scroll-direction
-                                   #(angle-point
-                                     (+ (point-angle %)
-                                        (* c/scroll-speed delta)))))
-    (let [{:keys [mouse page]} @app-state]
-      (when (and (:down? mouse)
-                 (mouse-dragging?)
-                 (= (:down-zone mouse) :empty))
-        (page-action page :scroll
-                     (-
-                      (/ (scalar-point-projection (subtract-points mouse
-                                                                             (:last-pos mouse))
-                                                       (global-attr :scroll-direction))
-                         (* (base-zoom)
-                            c/outer-form-spacing)))
-                     (assoc mouse :dragging? (mouse-dragging?)))))
-    (all-pages-action :update delta (attr :mouse) (get-mouse-zone))
-    (update-attr! :mouse
-                  #(assoc %
-                          :last-pos
-                          (select-keys % [:x :y])))
+                           #(angle-point
+                             (+ (point-angle %)
+                                (* c/scroll-speed delta)))))
+    (when (and (:down? @mouse)
+               (mouse-dragging?)
+               (= (:down-zone @mouse) :empty))
+      (page-action @active-page :scroll
+                   (-
+                    (/ (scalar-point-projection (subtract-points @mouse
+                                                                 (:last-pos @mouse))
+                                                (global-attr :scroll-direction))
+                       (* (base-zoom)
+                          c/outer-form-spacing)))
+                   (assoc @mouse :dragging? (mouse-dragging?))))
+    (all-pages-action :update delta @mouse (get-mouse-zone))
+    (swap! mouse
+           #(assoc %
+                   :last-pos
+                   (select-keys % [:x :y])))
     (resize-html-elements)
     (render-app-state)))
 
@@ -229,27 +215,26 @@
         size (app-size)
         current-pos {:x (/ (- x (* 0.5 (- width size))) size)
                      :y (/ (- y (* 0.5 (- height size))) size)}]
-    (update-attr! :mouse
-                  (fn [state]
-                    (let [diff (subtract-points state
-                                                     current-pos)]
-                      (assoc (merge state current-pos)
-                             :drag-dist (when (:down? state)
-                                          (+ (point-magnitude diff)
-                                             (:drag-dist state)))))))))
+    (swap! mouse
+           (fn [mouse-state]
+             (let [diff (subtract-points mouse-state
+                                         current-pos)]
+               (assoc (merge mouse-state current-pos)
+                      :drag-dist (when (:down? mouse-state)
+                                   (+ (point-magnitude diff)
+                                      (:drag-dist mouse-state)))))))))
 
 (defn on-click-down [event]
   (update-mouse-pos event)
-  (let [{:keys [mouse]} @app-state
-        zone (get-mouse-zone)]
-    (update-attr! :mouse
-                  (fn [state]
-                    (assoc state
-                           :down-pos (select-keys mouse [:x :y])
-                           :down? true
-                           :down-zone zone
-                           :down-formbar-form-path (formbar-form-path-at mouse))))
-    (page-action (attr :page) :click-down mouse zone)))
+  (let [zone (get-mouse-zone)]
+    (swap! mouse
+           (fn [mouse-state]
+             (assoc mouse-state
+                    :down-pos (select-keys mouse-state [:x :y])
+                    :down? true
+                    :down-zone zone
+                    :down-formbar-form-path (formbar-form-path-at mouse-state))))
+    (page-action @active-page :click-down @mouse zone)))
 
 (defn refresh-html-colors []
   (all-pages-action :refresh-html-colors)
@@ -262,23 +247,22 @@
 
 (defn on-click-up [event]
   (update-mouse-pos event)
-  (let [{:keys [mouse]} @app-state
-        currently-dragging? (mouse-dragging?)
+  (let [currently-dragging? (mouse-dragging?)
         mouse-zone (get-mouse-zone)]
-    (page-action (attr :page)
+    (page-action @active-page
                  :click-up
-                 (assoc mouse
+                 (assoc @mouse
                         :dragging? (mouse-dragging?))
                  mouse-zone)
-    (update-attr! :mouse
-                  (fn [state]
-                    (assoc state
-                           :down? false
-                           :drag-dist 0)))
+    (swap! mouse
+           (fn [mouse-state]
+             (assoc mouse-state
+                    :down? false
+                    :drag-dist 0)))
     (when-not currently-dragging?
-      (case (:down-zone mouse)
+      (case (:down-zone @mouse)
         :formbar
-        (let [form-path (take 3 (formbar-form-path-at (:down-pos mouse)))
+        (let [form-path (take 3 (formbar-form-path-at (:down-pos @mouse)))
               {:keys [tool-type]} (get-in (project-attr :formbars) form-path)]
           (case tool-type
             :undo (undo!)
@@ -292,13 +276,13 @@
         (enter-page :text)
 
         :new-formbar
-        (add-project-formbar-at (new-formbar-circle-path-at mouse))
+        (add-project-formbar-at (new-formbar-circle-path-at @mouse))
 
         nil))))
 
 (defn init []
   (u/log "App Initializing...")
-  (set-attr! :page :code)
+  (reset! active-page :code)
 
   (doseq [action [:refresh-html-colors :resize-html]]
     (all-pages-action action))
